8719d04ae51a231f3cf9978ced4a7544d75fb9b5
Jesus Marron <jesusmarron7@gmail.com>
2023-06-26 16:30:06 -0500

    NotAuthorizedException added

---
+++ src/main/java/com/cooksys/group01/exception/NotAuthorizedException.java
@@ -0,0 +1,18 @@
+package com.cooksys.group01.exception;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.io.Serial;
+
+@AllArgsConstructor
+@Getter
+@Setter
+public class NotAuthorizedException extends RuntimeException{
+
+    @Serial
+    private static final long serialVersionUID = 6866344519140233671L;
+
+    private String message;
+}
