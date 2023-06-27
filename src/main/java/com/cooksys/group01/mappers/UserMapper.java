a12d718c3e33f18d5a0169eba758b7ee63e0c68c
Helena Makendengue <HelenaMakens1013@gmail.com>
2023-06-27 10:00:05 -0500

    Update UserMapper.java

--- src/main/java/com/cooksys/group01/mappers/UserMapper.java
+++ src/main/java/com/cooksys/group01/mappers/UserMapper.java
@@ -7,7 +7,7 @@ import org.mapstruct.Mapper;
 
 import java.util.List;
 
-@Mapper(componentModel = "spring")
+@Mapper(componentModel = "spring", uses = {CredentialMapper.class, ProfileMapper.class})
 public interface UserMapper {
 
     UserRespDTO entityToDTO(User user);
