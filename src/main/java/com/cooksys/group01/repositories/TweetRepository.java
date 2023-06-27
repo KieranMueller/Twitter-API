4c7bd0ebf57b86d9c477bb3dd72b2ad92ae647ee
Helena Makendengue <HelenaMakens1013@gmail.com>
2023-06-27 10:02:14 -0500

    Update TweetRepository.java

--- src/main/java/com/cooksys/group01/repositories/TweetRepository.java
+++ src/main/java/com/cooksys/group01/repositories/TweetRepository.java
@@ -6,6 +6,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 import java.util.Optional;
 
+import org.springframework.stereotype.Repository;
+
+@Repository
 public interface TweetRepository extends JpaRepository<Tweet, Long> {
 
     Optional<Tweet> findByIdAndDeletedFalse(Long id);
