1a651dacde55114ed5b6fba46d67049ab034d62c
KieranMueller <kieran98mueller@gmail.com>
2023-06-27 19:08:22 -0500

    Finished getRandomHashtag

--- src/main/java/com/cooksys/group01/controllers/HashtagController.java
+++ src/main/java/com/cooksys/group01/controllers/HashtagController.java
@@ -3,8 +3,10 @@ package com.cooksys.group01.controllers;
 import com.cooksys.group01.dtos.HashtagDTO;
 import com.cooksys.group01.services.HashtagService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -15,6 +17,7 @@ public class HashtagController {
     private final HashtagService hashtagService;
 
     @GetMapping("/randomTag")
+    @ResponseStatus(HttpStatus.TEMPORARY_REDIRECT)
     public HashtagDTO getRandomHashtag() {
         return hashtagService.getRandomHashtag();
     }
--- src/main/java/com/cooksys/group01/services/impl/HashtagServiceImpl.java
+++ src/main/java/com/cooksys/group01/services/impl/HashtagServiceImpl.java
@@ -2,23 +2,25 @@ package com.cooksys.group01.services.impl;
 
 import com.cooksys.group01.dtos.HashtagDTO;
 import com.cooksys.group01.entities.Hashtag;
+import com.cooksys.group01.mappers.HashtagMapper;
 import com.cooksys.group01.repositories.HashtagRepository;
 import com.cooksys.group01.services.HashtagService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Random;
 
 @Service
 @RequiredArgsConstructor
 public class HashtagServiceImpl implements HashtagService {
 
     private final HashtagRepository hashtagRepository;
+    private final HashtagMapper hashtagMapper;
 
     @Override
     public HashtagDTO getRandomHashtag() {
         List<Hashtag> tags = hashtagRepository.findAll();
-
-        return null;
+        return hashtagMapper.entityToDTO(tags.get(new Random().nextInt(tags.size())));
     }
 }
