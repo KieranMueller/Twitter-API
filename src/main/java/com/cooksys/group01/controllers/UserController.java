2a19108b5fad4334b79b0b92d834fce9a78ad4ae
KieranMueller <kieran98mueller@gmail.com>
2023-06-28 17:32:01 -0500

    Me and Josh tackled DELETE users/@{username} endpoint, passing all tests. Cleaned up controllers (removed optional forward slashes from endpoint strings)

--- src/main/java/com/cooksys/group01/controllers/UserController.java
+++ src/main/java/com/cooksys/group01/controllers/UserController.java
@@ -1,5 +1,6 @@
 package com.cooksys.group01.controllers;
 
+import com.cooksys.group01.dtos.CredentialsDTO;
 import com.cooksys.group01.dtos.TweetRespDTO;
 import com.cooksys.group01.dtos.UserReqDTO;
 import com.cooksys.group01.dtos.UserRespDTO;
@@ -14,7 +15,7 @@ import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/users")
+@RequestMapping("users")
 public class UserController {
 
     private final UserService userService;
@@ -24,37 +25,35 @@ public class UserController {
         return userService.getActiveUsers();
     }
   
-    @GetMapping("/@{username}")
+    @GetMapping("@{username}")
     public UserRespDTO getUser(@PathVariable String username) {
     	return userService.getUser(username);
     }
-   
 
-    @GetMapping("/@{username}/followers")
+    @GetMapping("@{username}/followers")
     public List<UserRespDTO> getFollowers(@PathVariable String username) {
         return userService.getFollowers(username);
     }
 
-    @GetMapping("/@{username}/following")
+    @GetMapping("@{username}/following")
     public List<UserRespDTO> getFollowing(@PathVariable String username) {
         return userService.getFollowing(username);
     }
 
-    @GetMapping("/@{username}/mentions")
+    @GetMapping("@{username}/mentions")
     public List<TweetRespDTO> getMentions(@PathVariable String username) {
         return userService.getMentions(username);
     }
 
-    @GetMapping("/@{username}/feed")
+    @GetMapping("@{username}/feed")
     public List<TweetRespDTO> getFeed(@PathVariable String username) {
         return userService.getFeed(username);
     }
 
-    @PatchMapping("/@{username}")
+    @PatchMapping("@{username}")
     public UserRespDTO updateUser(@PathVariable String username, @RequestBody UserReqDTO user) {
         return userService.updateUser(username, user);
     }
-
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
@@ -62,14 +61,19 @@ public class UserController {
         return userService.createUser(user);
     }
 
-    @PostMapping("/@{username}/follow")
+    @PostMapping("@{username}/follow")
     public void followUser(@PathVariable String username, @RequestBody Credentials credentials) {
         userService.followUser(username, credentials);
     }
 
-    @PostMapping("/@{username}/unfollow")
+    @PostMapping("@{username}/unfollow")
     public void unfollowUser(@PathVariable String username, @RequestBody Credentials credentials) {
         userService.unfollowUser(username, credentials);
+    }
+
+    @DeleteMapping("@{username}")
+    public UserRespDTO deleteUser(@PathVariable String username, @RequestBody CredentialsDTO credentials) {
+        return userService.deleteUser(username, credentials);
     }
 
 }
